044b23b30fea71e5fa1ff3813fafb0ddcaca5ae1
Travo100 <travo100@gmail.com>
2019-04-16 17:48:14 -0700

    added MONGODB_URI

--- server.js
+++ server.js
@@ -15,8 +15,10 @@ app.use(express.json());
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scarperData";
+
 mongoose
-    .connect("mongodb://localhost/scarperData", { useNewUrlParser: true }, (err) => {
+    .connect(MONGODB_URI, { useNewUrlParser: true }, (err) => {
         if(err) throw err;
         console.log("Database Connected!");
     });
